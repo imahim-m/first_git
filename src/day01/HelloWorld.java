b047e282af537e4b49c5a3d71a1ce66397e2a089
mahym <mahmamedova@gmail.com>
2025-02-27 00:51:41 -0500

    HelloWorld updated

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,6 +3,7 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hi");
+        System.out.println("Hi")
+        ;
     }
 }
